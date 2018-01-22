f3f0365ac72c828287f84798c3749bfaa7f2085e
Roger Hunwicks <roger@tonic-solutions.com>
2018-01-22 09:40:04 +0100

    Create ReadGeoDataFrameTask - see #8908

--- tasks.py
+++ tasks.py
@@ -77,6 +77,31 @@ class ReadDataFrameTask(Task):
             self.output().put(output)
 
 
+class ReadGeoDataFrameTask(ReadDataFrameTask):
+    """
+    A Pipeline Task that reads one or more GeoPandas GeoDataFrames from file(s) and returns them as a MemoryTarget
+    """
+    extensions = None
+
+    def read_dataframe(self, file_obj):
+        with file_obj.open('r') as f:
+            filename = f.name
+            gdf = gpd.read_file(filename, **self.get_read_args())
+            return gdf
+
+    def run(self):
+        targets = self.input()
+
+        if isinstance(targets, luigi.Target):
+            self.output().put(self.read_dataframe(targets))
+        elif isinstance(targets, dict):
+            output = {key: self.read_dataframe(target) for key, target in targets.items()}
+            self.output().put(output)
+        elif isinstance(targets, (list, tuple)):
+            output = {os.path.basename(target.path): self.read_dataframe(target) for target in targets}
+            self.output().put(output)
+
+
 class DataFrameOutputTask(Task):
     """
     Base class for all dataframe outputs.
